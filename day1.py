7500559560c6a49dfe294377ce9061f4ec85c78f
Cory Paszul <cpaszul@gmail.com>
2022-12-01 12:49:43 -0500

    Removed unneeded list()

--- day1.py
+++ day1.py
@@ -2,7 +2,7 @@ DEFAULT_INPUT = 'day1.txt'
 
 def day_1(loc: str = DEFAULT_INPUT) -> tuple[int, int]:
     with open(loc) as f:
-        elves = [sum(list(map(int, elf.split('\n')))) for elf in f.read().split('\n\n')]
+        elves = [sum(map(int, elf.split('\n'))) for elf in f.read().split('\n\n')]
     elves.sort(reverse=True)
     return elves[0], sum(elves[:3])
 
