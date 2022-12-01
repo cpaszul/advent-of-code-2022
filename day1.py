23fc7413d6678865c7335b3835f3a0f5ca19a981
Cory Paszul <cpaszul@gmail.com>
2022-12-01 00:07:48 -0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,12 @@
+DEFAULT_INPUT = 'day1.txt'
+
+def day_1(loc: str = DEFAULT_INPUT) -> tuple[int, int]:
+    with open(loc) as f:
+        elves = [sum(list(map(int, elf.split('\n')))) for elf in f.read().split('\n\n')]
+    elves.sort(reverse=True)
+    return elves[0], sum(elves[:3])
+
+if __name__ == '__main__':
+    part_1, part_2 = day_1()
+    print('Solution for Part One:', part_1)
+    print('Solution for Part Two:', part_2)
